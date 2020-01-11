ec9156115fb47f7047976b96616d5c14936566c7
tyelf22 <tyelf22@hotmail.com>
2020-01-11 02:49:37 -0700

    added asyc await, dropdown for ability

--- main.js
+++ main.js
@@ -1,76 +1,65 @@
-/* function createPokemon(id) {
 
-    let url = `https://pokeapi.co/api/v2/pokemon/${id}/`
-    let ul = document.querySelector('#list')
-
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        let name = data.name
-        let sprites = data.sprites.front_default
-        console.log(sprites)
-    
-        let li = document.createElement('li')
-        let myImage = document.createElement('img')
-        
-        myImage.src = sprites
-        li.textContent = name
-        ul.appendChild(li)
-        li.appendChild(myImage)
-
-    })
-}
-
-function pokemonIterate(start, stop) {
-let i = start
-while (i <= stop) {
-    createPokemon(i);
-    i++;
-}
-}
-
-pokemonIterate(1, 5);
-
+let offset = 0
 let nextBtn = document.querySelector('#nextBtn')
 nextBtn.addEventListener('click', () => {
-    pokemonIterate(5, 10)
-}) */
-
-    let offset = 0
-    let nextBtn = document.querySelector('#nextBtn')
-    nextBtn.addEventListener('click', () => {
-        offset += 5
-        createPokemon()
-    })
+    offset += 5
+    createPokemon()
+})
 
 
-function createPokemon() {
+async function createPokemon() {
 
     let urlOne = `https://pokeapi.co/api/v2/pokemon?limit=5&offset=${offset}`
     let ul = document.querySelector('#list')
 
-    fetch(urlOne)
+    await fetch(urlOne)
     .then(response => response.json())
     .then(data => {
         let results = data.results
-
-        results.map(result => {
+        
+         results.map(result => {
            let pokeUrl = result.url 
-           fetch(pokeUrl)
+          fetch(pokeUrl)
            .then(response => response.json())
            .then(moreData => {
+               console.log(moreData)
                let name = moreData.name
+
+
                let sprites = moreData.sprites.front_default
               
            
                let li = document.createElement('li')
+               li.style.cursor = 'pointer'
+              
+
+              
+                let ability = moreData.abilities[0].ability.name
+                
+                let newAbility = document.createElement('p')
+
+                newAbility.textContent = `Ability: ${ability}`
+
+                newAbility.style.display = 'none';
+
+                //show pokemon ability
+                li.addEventListener('click', () => {
+                    if(newAbility.style.display === "none") {
+                        newAbility.style.display = "block"
+                    } else {
+                        newAbility.style.display = "none"
+                    }
+                })
+
+
                let myImage = document.createElement('img')
                
                myImage.src = sprites
                li.textContent = name
+               li.innerHTML = name + " <span><i class='fa fa-caret-down'></i></span>"
                ul.appendChild(li)
                li.appendChild(myImage)
+               ul.appendChild(newAbility)
            })
         })
         
@@ -79,39 +68,6 @@ function createPokemon() {
 
 createPokemon();
 
-
-
-/*     .then(data => {
-        
-        let name = data.name
-        let sprites = data.sprites.front_default
-       
-    
-        let li = document.createElement('li')
-        let myImage = document.createElement('img')
-        
-        myImage.src = sprites
-        li.textContent = name
-        ul.appendChild(li)
-        li.appendChild(myImage)
-
-    })
-}
-
-function pokemonIterate(start, stop) {
-let i = start
-while (i <= stop) {
-    createPokemon(i);
-    i++;
-}
-}
-
-pokemonIterate(1, 5);
-
-let nextBtn = document.querySelector('#nextBtn')
-nextBtn.addEventListener('click', () => {
-    pokemonIterate(5, 10)
-}) */
 
 
 
